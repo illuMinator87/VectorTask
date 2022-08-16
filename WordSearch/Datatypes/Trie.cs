78d8d6a29c176426ffa9491c156ba8502858643b
Manuel <seifert.manuel@outlook.com>
2022-08-16 13:20:08 +0200

    Add Trie class

--- WordSearch/Datatypes/Trie.cs
+++ WordSearch/Datatypes/Trie.cs
@@ -1,18 +1,27 @@
 ﻿namespace Datatypes
 {
+    public class Node
+    {
+        public Dictionary<char, Node> Children { get; set; }
+        public bool IsLastCharOfWord { get; set; }
+        public Mutex Mutex { get; set; }
+
+        public Node()
+        {
+            Children = new Dictionary<char, Node>();
+            IsLastCharOfWord = false;
+        }
+    }
+
     public class Trie
     {
-        public class Node
-        {
-            public Dictionary<char, Node> Children { get; set; }
-            public bool IsLastCharOfWord { get; set; }
-            public Mutex Mutex { get; set; }
+        public Node RootNode { get; private set; }
 
-            public Node()
-            {
-                Children = new Dictionary<char, Node>();
-                IsLastCharOfWord = false;
-            }
+        public Trie()
+        {
+            RootNode = new Node();
         }
+
+
     }
 }
