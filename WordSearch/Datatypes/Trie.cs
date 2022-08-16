82327a41905f4d32ac68f35829ea70f27ffcad89
Manuel <seifert.manuel@outlook.com>
2022-08-16 13:21:46 +0200

    Implement search by prefix

--- WordSearch/Datatypes/Trie.cs
+++ WordSearch/Datatypes/Trie.cs
@@ -36,5 +36,38 @@
             }
             currentNode.IsLastCharOfWord = true;
         }
+
+        public List<string> FindWordsWithPrefix(string prefix)
+        {
+            Node currentNode = RootNode;
+
+            foreach (char letter in prefix)
+            {
+                if (!currentNode.Children.ContainsKey(letter))
+                {
+                    return new List<string>();
+                }
+                currentNode = currentNode.Children[letter];
+            }
+
+            List<string> searchResult = new();
+            GetAllWordsForNodeRecursive(prefix, currentNode, ref searchResult);
+
+            return searchResult;
+        }
+
+        private void GetAllWordsForNodeRecursive(string prefix, Node startNode, ref List<string> searchResult)
+        {
+            foreach (char nodeChar in startNode.Children.Keys)
+            {
+                if (startNode.Children[nodeChar].IsLastCharOfWord)
+                {
+                    searchResult.Add(prefix + nodeChar);
+                    continue;
+                }
+
+                GetAllWordsForNodeRecursive(prefix + nodeChar, startNode.Children[nodeChar], ref searchResult);
+            }
+        }
     }
 }
