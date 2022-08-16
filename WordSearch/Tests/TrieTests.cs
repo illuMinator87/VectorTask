66670dc66600ec7c90782194a06d5eab3955f20e
Manuel <seifert.manuel@outlook.com>
2022-08-16 13:59:08 +0200

    Add unit tests for Trie

--- WordSearch/Tests/TrieTests.cs
+++ WordSearch/Tests/TrieTests.cs
@@ -1,7 +1,35 @@
-﻿namespace Tests
+﻿using Datatypes;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Tests
 {
     public class TrieTests
     {
+        [Test]
+        public void TestInsertAndFindWords()
+        {
+            List<string> inputWords = new() { "test", "testing", "trie", "trietest", "banana"};
 
+            Trie trie = new();
+            foreach (string word in inputWords)
+            {
+                trie.InsertWord(word);
+            }
+
+            List<string> result = trie.FindWordsWithPrefix("test");
+            result.Should().HaveCount(2, $"the words 'test' and 'testing' should match the prefix 'test'");
+            result[0].Should().Be("test");
+            result[1].Should().Be("testing");
+
+            result = trie.FindWordsWithPrefix("tr");
+            result.Should().HaveCount(2, $"the words 'trie' and 'trietest' should match the prefix 'tr'");
+            result[0].Should().Be("trie");
+            result[1].Should().Be("trietest");
+
+            result = trie.FindWordsWithPrefix("b");
+            result.Should().HaveCount(1, $"the word 'banana' should match the prefix 'b'");
+            result[0].Should().Be("banana");
+        }
     }
 }
