40e34ddcd0e5f0a1059c0265f3f99c62e3f8c333
Manuel <seifert.manuel@outlook.com>
2022-08-16 14:03:57 +0200

    Add function to test in console app

--- WordSearch/WordSearch/Program.cs
+++ WordSearch/WordSearch/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace WordSearch
+﻿using Datatypes;
+using System.Diagnostics;
+
+namespace WordSearch
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            List<string> testWords = GenerateTestWords();
+            Console.WriteLine($"Generated {testWords.Count} test words");
+
+            Stopwatch watch = Stopwatch.StartNew();
+            Trie searchTrie = new Trie();
+            foreach (string word in testWords)
+            {
+                searchTrie.InsertWord(word);
+            }
+            watch.Stop();
+            Console.WriteLine($"Building Search Trie took {watch.Elapsed.TotalMilliseconds} milliseconds");
+
+            string searchString = "AA";
+            Console.WriteLine($"Searching for substring {searchString}");
+
+            watch.Restart();
+            List<string> searchResult = searchTrie.FindWordsWithPrefix(searchString);
+            watch.Stop();
+
+            Console.WriteLine($"Done Searching... Result:");
+
+            foreach (string resultWord in searchResult)
+            {
+                Console.WriteLine(resultWord);
+            }
+
+            Console.WriteLine($"Search took {watch.Elapsed.TotalMilliseconds} milliseconds");
         }
 
         public static List<string> GenerateTestWords()
