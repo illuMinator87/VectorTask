7e266d45ceb95e6d62f4db53f0d214abd4e91804
Manuel <seifert.manuel@outlook.com>
2022-08-16 13:18:38 +0200

    Add Node class

--- WordSearch/Datatypes/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Datatypes
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ WordSearch/Datatypes/Trie.cs
@@ -0,0 +1,18 @@
+﻿namespace Datatypes
+{
+    public class Trie
+    {
+        public class Node
+        {
+            public Dictionary<char, Node> Children { get; set; }
+            public bool IsLastCharOfWord { get; set; }
+            public Mutex Mutex { get; set; }
+
+            public Node()
+            {
+                Children = new Dictionary<char, Node>();
+                IsLastCharOfWord = false;
+            }
+        }
+    }
+}
