655b7cce135c7ab869e1fc1a307138d93a670678
Manuel <seifert.manuel@outlook.com>
2022-08-16 14:01:34 +0200

    Add function to create test words

--- WordSearch/WordSearch/Program.cs
+++ WordSearch/WordSearch/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace WordSearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static List<string> GenerateTestWords()
+        {
+            List<string> words = new();
+
+            char[] alphabet = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                for (int j = 0; j < alphabet.Length; j++)
+                {
+                    for (int k = 0; k < alphabet.Length; k++)
+                    {
+                        for (int l = 0; l < alphabet.Length; l++)
+                        {
+                            words.Add($"{alphabet[i]}{alphabet[j]}{alphabet[k]}{alphabet[l]}");
+                        }
+                    }
+                }
+            }
+
+            Random random = new();
+            List<string> randomizedOrder = words.OrderBy(word => random.Next()).ToList();
+
+            return randomizedOrder;
+        }
+    }
+}
